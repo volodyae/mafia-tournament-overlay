fdeb00e371a84ea323cef1d655da47772edfb459
volodyae <volodyae7@gmail.com>
2026-02-20 20:27:52 +0300

    Правлен config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.OVERLAY_CONFIG = {
-  API_URL: window.OVERLAY_API_URL || 'http://192.168.0.121:3000/api',
-  SOCKET_URL: window.OVERLAY_SOCKET_URL || 'http://192.168.0.121:3000',
-  BASE_URL: window.OVERLAY_BASE_URL || 'http://192.168.0.121:3000'
+  API_URL: window.OVERLAY_API_URL || 'https://mafia-tournament-overlay.onrender.com/api',
+  SOCKET_URL: window.OVERLAY_SOCKET_URL || 'https://mafia-tournament-overlay.onrender.com',
+  BASE_URL: window.OVERLAY_BASE_URL || 'https://mafia-tournament-overlay.onrender.com'
 };
